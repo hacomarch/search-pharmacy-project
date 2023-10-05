74dbecb922301b01aa5745418b8fb463bfec9cac
윤하은 <haco_march@naver.com>
2023-10-05 22:43:01 +0900

    카카오 주소 api에서 받은 response를 받기 위해 dto들 생성

---
+++ src/main/java/com/example/searchpharmacyproject/api/dto/DocumentDto.java
@@ -0,0 +1,25 @@
+package com.example.searchpharmacyproject.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/*
+* 카카오 주소 api에서 받은 response에서
+* document 부분에서 주소와 위도, 경도를 받아와 dto로 저장
+*/
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DocumentDto {
+
+    @JsonProperty("address_name")
+    private String addressName;
+
+    @JsonProperty("y")
+    private double latitude; //위도
+
+    @JsonProperty("x")
+    private double longitude; //경도
+}
