0fd54bf1c0ba6f22d8abf651650a89a9c5cb62d5
윤하은 <haco_march@naver.com>
2023-10-05 15:25:45 +0900

    initial commit

---
+++ src/main/java/com/example/searchpharmacyproject/SearchPharmacyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.searchpharmacyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchPharmacyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchPharmacyProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/searchpharmacyproject/SearchPharmacyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.searchpharmacyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchPharmacyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
