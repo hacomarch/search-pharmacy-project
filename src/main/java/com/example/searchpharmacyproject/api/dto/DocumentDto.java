29dbd130285b9aabcc87b87941a668e8c36127ce
윤하은 <haco_march@naver.com>
2023-10-06 21:04:04 +0900

    카카오 주소 중 카테고리 검색 api로 교체한다.
    기존에 하버사인 알고리즘을 사용해 주변 약국을 구하는 코드를 작성했으나,
    
    카카오에서 약국(PM9) 이라는 카테고리를 넣으면 반경 몇 km 이내에 있는 약국을 보여주는 api로 교체했다.
    그에 따른 코드를 변경하고 테스트를 진행했다.

--- src/main/java/com/example/searchpharmacyproject/api/dto/DocumentDto.java
+++ src/main/java/com/example/searchpharmacyproject/api/dto/DocumentDto.java
@@ -16,6 +16,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class DocumentDto {
 
+    @JsonProperty("place_name")
+    private String placeName;
+
     @JsonProperty("address_name")
     private String addressName;
 
@@ -24,4 +27,7 @@ public class DocumentDto {
 
     @JsonProperty("x")
     private double longitude; //경도
+
+    @JsonProperty("distance")
+    private double distance;
 }
