343914755087a69ce8a38939494215acfdad3572
윤하은 <haco_march@naver.com>
2023-10-06 15:06:21 +0900

    추천받은 약국 리스트를 가져오는 서비스
    사용자의 주소를 입력해서 반경 10km 이내의 약국 3개를 받아온다.
    
    그 약국 리스트를 가져와 DB에 저장하는 서비스를 구현하고 테스트했다.
    
    spock 테스트에서 where: 에 대한 공부도 해봤다.

---
+++ src/main/java/com/example/searchpharmacyproject/pharmacy/service/PharmacyRecommendationService.java
@@ -0,0 +1,44 @@
+package com.example.searchpharmacyproject.pharmacy.service;
+
+import com.example.searchpharmacyproject.api.dto.DocumentDto;
+import com.example.searchpharmacyproject.api.dto.KakaoApiResponseDto;
+import com.example.searchpharmacyproject.api.service.KakaoAddressSearchService;
+import com.example.searchpharmacyproject.direction.entity.Direction;
+import com.example.searchpharmacyproject.direction.service.DirectionService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PharmacyRecommendationService {
+
+    private final KakaoAddressSearchService kakaoAddressSearchService;
+    private final DirectionService directionService;
+
+    public void recommendPharmacyList(String address) {
+        KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
+
+        //validation check
+        if (Objects.isNull(kakaoApiResponseDto) || CollectionUtils.isEmpty(kakaoApiResponseDto.getDocumentList())) {
+            log.error("[PharmacyRecommendationService.recommendPharmacyList fail] Input address: {}", address);
+            return;
+        }
+
+        //사용자가 입력한 주소를 dto로 변환해서 가져옴
+        DocumentDto documentDto = kakaoApiResponseDto.getDocumentList().get(0);
+        //사용자 위치에서 가까운 약국을 리스트로 가져와서 DB에 저장
+        List<Direction> directionList = directionService.buildDirectionList(documentDto);
+        directionService.saveAll(directionList);
+
+
+
+    }
+
+}
